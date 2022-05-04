684ebb47ad996989badbe40a23b8fe638773d1c4
abdurahmoniy <96695070+abdurahmoniy@users.noreply.github.com>
2022-05-04 15:25:47 +0500

    A

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { AntLayout } from './components/Layout/Layout'
 
 function App() {
   return (
     <div className="App">
-      Azizbek
+      <AntLayout />
     </div>
   );
 }
